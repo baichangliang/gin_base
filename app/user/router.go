d6b510acf5717f0142708998129688c0c8f1f1c4
root <13520547055@163.com>
2021-11-17 10:37:58 +0800

    路由分包后分组

--- app/user/router.go
+++ app/user/router.go
@@ -3,5 +3,15 @@ package user
 import "github.com/gin-gonic/gin"
 
 func Routers(e *gin.Engine) {
-	e.GET("/api/", GetWxOpenId)
+	// 不需要登陆权限
+	unauthorized := e.Group("/api/user_un/")
+	{
+		unauthorized.GET("", GetWxOpenId) //
+	}
+
+	// pc端, 需要公司人员认证权限
+	pcUser := e.Group("/api/user_pc/", AuthMiddleware("Bearer "))
+	{
+		pcUser.GET("", GetWxOpenId)
+	}
 }
